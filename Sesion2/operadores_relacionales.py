48e5b424b4bfcd97fe9e809c25ffdaa04145dc6e
Marcelo <mamojica@uamv.edu.ni>
2026-08-19 08:49:19 -0600

    agrego dos funciones mas

---
+++ Sesion2/operadores_relacionales.py
@@ -0,0 +1,10 @@
+#Leer dos nuemros y compararlos
+number1 = int(input("Ingresa un #: "))
+number2 = int(input("Ingresa otro #: "))
+
+print(f"{number1} > {number2} = {number1 > number2}")
+print(f"{number1} < {number2} = {number1 < number2}")
+print(f"{number1} >= {number2} = {number1 >= number2}")
+print(f"{number1} <= {number2} = {number1 <= number2}")
+print(f"{number1} == {number2} = {number1 == number2}")
+print(f"{number1} != {number2} = {number1 != number2}")
